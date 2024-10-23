f8b86ee84b78bbdc40a443969f8fd2481914affa
mitesh-avid-brio <mitesh@avidbrio.com>
2024-10-23 18:49:56 +0530

    new test

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: process.env.NEXT_PUBLIC_API_URL,
+        hostname: process.env.NEXT_PUBLIC_WORDPRESS_HOSTNAME,
         port: "",
         pathname: "/**",
       },
